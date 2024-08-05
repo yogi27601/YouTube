79bfede43a37841588182d7ea113a23a593e93b5
yogi27601 <yogi27601@gmail.com>
2024-08-05 03:35:58 -0700

    Update model.js

--- models/model.js
+++ models/model.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://yogi27601:Rahul%4027601@cluster0.ztpvtfm.mongodb.net/videodb')
+mongoose.connect('mongodb+srv://yogi27601:Rahul%4027601@cluster0.ztpvtfm.mongodb.net/videoDB')
   .then(() => {
     console.log('Connected to MongoDB');
   })
